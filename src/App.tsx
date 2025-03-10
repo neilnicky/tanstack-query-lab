6635147a6e6e773d866043f71c2f5b18d9d885e1
neilnicky <neilnicholaskanat@gmail.com>
2025-03-10 17:20:24 +0530

    feat: usequery data, ispending, refetch, error

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
+import { Loader } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isPending, refetch, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
+  if (error) {
+    alert("Something went wrong");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{isPending ? <Loader /> : JSON.stringify(data?.slice(0, 20))}</div>
+      <button onClick={() => refetch()}>refetch</button>
     </>
-  )
+  );
 }
 
-export default App
+const getTodos = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch("http://jsonplaceholder.typicode.com/todos");
+  return await response.json();
+};
+
+export default App;

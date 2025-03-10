f7593363323f76624792841e203ed1d2eedecdc3
Neil Nicholas <127874454+neilnicky@users.noreply.github.com>
2025-03-10 17:34:56 +0530

    feat: loading states, error states, refetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import { useState } from "react";
+
 
 export default function Home() {
+ const [id, setId] = useState(1);
+  const { data, isPending, error } = useQuery({
+    queryKey: ["todos", id],
+    queryFn: () => getTodos(id),
+  });
+
+  if (error) {
+    alert("Something went wrong");
+  }
+
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <div>{isPending ? <Loader /> : JSON.stringify(data?.slice(0, 20))}</div>
+      <button onClick={() => setId((prev) => prev + 1)}>Increment ID</button>
+    </>
   );
 }
+
+const getTodos = async (id: number) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${id}`
+  );
+  return await response.json();
+};

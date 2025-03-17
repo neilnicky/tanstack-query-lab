08e08a08abd42eeec40055a61a5e2cad999f5e85
neilnicky <neilnicholaskanat@gmail.com>
2025-03-17 13:23:45 +0530

    fix: query parameters

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Suspense } from "react";
 import "./App.css";
 import Card from "./components/Card";
-import CardX from "./components/CardX";
 
 export default function App() {
   return (
     <>
       <Suspense fallback={<div>Varumm...</div>}>
-        <CardX />
+        <Card />
       </Suspense>
     </>
   );
